76f9e48a28b39c04bee9f91c3ec8b194e2d168ff
dle-fr <dle-fr>
2024-12-05 23:24:05 +0100

    day 5

--- src/main/java/org/dle/adventofcode2024/Day5.java
+++ src/main/java/org/dle/adventofcode2024/Day5.java
@@ -1,16 +1,69 @@
 package org.dle.adventofcode2024;
 
-import java.util.List;
+import java.util.*;
+import java.util.function.BiPredicate;
 
 class Day5 {
 
     long part1(List<String> input) {
-        List<String> strings = input != null ? input : AoCUtils.readFile(this);
-        return -1;
+        return filterFixAndReduce((update, rules) -> getBadIndex(update, rules) == -1, input); // searching for good updates only
     }
 
     long part2(List<String> input) {
+        return filterFixAndReduce((update, rules) -> getBadIndex(update, rules) >= 0, input); // searching for bad updates only
+    }
+
+    // Split input, filter good or bad updates, fix the order if necessary, get middle update value, sum
+    private long filterFixAndReduce(BiPredicate<List<Long>, List<List<Long>>> filterPredicate, List<String> input) {
         List<String> strings = input != null ? input : AoCUtils.readFile(this);
+
+        List<List<Long>> rules = strings.stream().filter(s -> s.contains("|")).map(s -> toLongs(s.split("\\|"))).toList();
+        List<List<Long>> updates = strings.stream().filter(s -> s.contains(",")).map(s-> toLongs(s.split(","))).toList();
+
+        return updates.parallelStream()
+                .filter(u -> filterPredicate.test(u, rules))
+                .map(u -> fixOrder(u, rules))
+                .map(u -> u.get(Math.floorDiv(u.size(), 2)))
+                .reduce(0L, Long::sum);
+    }
+
+    private List<Long> toLongs(String[] split) {
+        return Arrays.stream(split).map(Long::parseLong).toList();
+    }
+
+    // Should return a correct ordered update list, according to rules
+    private List<Long> fixOrder(List<Long> update, List<List<Long>> rules) {
+        List<Long> res = new ArrayList<>(update);
+        while(true) { // yeah, but in theory no possible ∞ loop
+            int i = getBadIndex(res, rules);
+            if (i >= 0) { // we have a bad index, let's swap the element with previous one (element is too far)
+                Long l = res.get(i);
+                res.set(i, res.get(i - 1));
+                res.set(i - 1, l);
+            } else {
+                return res; // 0 -> order is good!
+            }
+        }
+    }
+
+    // Returns -1 if correct order, otherwise, returns the index of the first wrong element (which is too far in list)
+    private int getBadIndex(List<Long> update, List<List<Long>> rules) {
+        // Searching for next numbers that are in the smallest rule part
+        for (int i = 0; i < update.size() - 1; i++) {
+            List<Long> subList = update.subList(i + 1, update.size());
+            int finalI = i; // necessary for stream filter (kinda "final")
+            // Loop on 2nd rules values equal to value at current update index, searching for 1st rules values
+            // present after current index, which would indicate a bad order
+            Optional<Integer> index = rules.parallelStream()
+                    .filter(r -> Objects.equals(r.get(1), update.get(finalI)))
+                    .map(r -> r.get(0))
+                    .map(subList::indexOf)
+                    .filter(ind -> ind > -1)
+                    .findAny();
+            if (index.isPresent()) {
+                return index.get() + i + 1; // index + i (offset from initial list) + 1 (sublist started at index + 1)
+            }
+        }
         return -1;
     }
-}
+} // Nice
--- src/test/java/org/dle/adventofcode2024/Day5Test.java
+++ src/test/java/org/dle/adventofcode2024/Day5Test.java
@@ -2,7 +2,7 @@ package org.dle.adventofcode2024;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,28 +11,54 @@ public class Day5Test {
     @Test
     void part1_test() {
         long res = new Day5().part1(null);
-        System.out.println(res);
-        assertEquals(0, res);
+        assertEquals(6951, res);
     }
 
     @Test
     void part2_test() {
         long res = new Day5().part2(null);
-        System.out.println(res);
-        assertEquals(0, res);
+        assertEquals(4121, res);
     }
+
+    private static final List<String> TEST_LIST = List.of("""
+            47|53
+            97|13
+            97|61
+            97|47
+            75|29
+            61|13
+            75|53
+            29|13
+            97|29
+            53|29
+            61|53
+            97|53
+            61|29
+            47|13
+            75|47
+            97|75
+            47|61
+            75|61
+            47|29
+            75|13
+            53|13
+            
+            75,47,61,53,29
+            97,61,53,29,13
+            75,29,13
+            75,97,47,61,53
+            61,13,29
+            97,13,75,29,47""".split("\n"));
 
     @Test
     void part1_test_example() {
-        long res = new Day5().part1(Arrays.asList());
-        System.out.println(res);
-        assertEquals(0, res);
+        long res = new Day5().part1(TEST_LIST);
+        assertEquals(143, res);
     }
 
     @Test
     void part2_test_example() {
-        long res = new Day5().part2(Arrays.asList());
-        System.out.println(res);
-        assertEquals(0, res);
+        long res = new Day5().part2(TEST_LIST);
+        assertEquals(123, res);
     }
 }
